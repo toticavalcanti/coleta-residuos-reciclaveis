fb6cba31253edfb9f1c86828797c789e7d7e3783
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-06-07 18:32:29 -0300

    leafmap

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style.css';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft  } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 const CreatePoint = () => {
+    useEffect(() => {
+        api.get('items').then(response => {
+            console.log(response);
+        });
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -57,13 +65,63 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa.</span>
                     </legend>
+
+                    <Map center={[-22.9518018, -43.1844011]} zoom={15}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                        />
+                        <Marker position={[-22.9518018, -43.1844011]} />
+                    </Map>                    
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade </option>
+                            </select>
+                        </div>
+                    </div>
                 </fieldset>
                 <fieldset>
                     <legend>
                         <h2>Ítens de Coleta</h2>
                         <span>Selecione um ou mais itens abaixo.</span>
                     </legend>
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li className="selected">
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                    </ul>
                 </fieldset>
+                <button type="submit">Cadastrar ponto de coleta</button>
             </form>
         </div>
     );
